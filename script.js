5f51e6a2f4d4febec741d73a9abc9ae11d058f68
unknown <ncmoore1986@gmail.com>
2017-08-08 15:17:47 -0700

    Now my script will accept y/n and not just yes/no

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ alert("Welcome to the EXTREME CHEDDAR guessing game!!");
 var score = 0;
 var tall = prompt("Is Nathan more than 20 feet tall?").toLowerCase();
 console.log(tall);
-if (tall == "yes" ) {
+if (tall == "yes" || tall == "y") {
   alert("INCORRECT!")
 }
-if (tall == "no" ) {
+if (tall == "no" || tall == "n") {
   alert("CORRECT!")
   score++
 }
@@ -15,10 +15,10 @@ console.log("Current score: " + score)
 
 var pickles = prompt("Does Nathan love pickles?").toLowerCase();
 console.log(pickles);
-if (pickles == "no" ) {
+if (pickles == "no" || pickles == "n") {
   alert("INCORRECT!")
 }
-if (pickles == "yes" ) {
+if (pickles == "yes" || pickles == "y") {
   alert("CORRECT!")
   score++
 }
@@ -26,10 +26,10 @@ console.log("Current score: " + score)
 
 var vampire = prompt("Is Nathan secretly a vampire?").toLowerCase();
 console.log(vampire);
-if (vampire == "yes" ) {
+if (vampire == "yes" || vampire == "y") {
   alert("INCORRECT!")
 }
-if (vampire == "no" ) {
+if (vampire == "no" || vampire == "n") {
   alert("CORRECT!")
   score++
 }
@@ -37,10 +37,10 @@ console.log("Current score: " + score)
 
 var helicopter = prompt("Can Nathan safely pilot a helicopter?").toLowerCase();
 console.log(helicopter);
-if (helicopter == "yes" ) {
+if (helicopter == "yes" || helicopter == "y") {
   alert("INCORRECT!")
 }
-if (helicopter == "no" ) {
+if (helicopter == "no" || helicopter == "n") {
   alert("CORRECT!")
   score++
 }
@@ -48,10 +48,10 @@ console.log("Current score: " + score)
 
 var mommy = prompt("Does Nathan love his mommy?").toLowerCase();
 console.log(mommy);
-if (mommy == "no" ) {
+if (mommy == "no" || mommy == "n") {
   alert("INCORRECT!")
 }
-if (mommy == "yes" ) {
+if (mommy == "yes" || mommy == "y") {
   alert("CORRECT!")
   score++
 }
