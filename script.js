4f304b1c3daa113f4a727fbcca5723e1c773150f
unknown <ncmoore1986@gmail.com>
2017-08-08 15:02:59 -0700

    Added the rest of the questions. Also the program now keeps score.

--- script.js
+++ script.js
@@ -1,11 +1,60 @@
 'use strict'
 
 alert("Welcome to the EXTREME CHEDDAR guessing game!!");
-var tall = prompt("Is Nathan 20 feet tall?");
-tall.toLowerCase();
-if (tall === "yes") {
+var score = 0;
+var tall = prompt("Is Nathan more than 20 feet tall?").toLowerCase();
+console.log(tall);
+if (tall == "yes" ) {
   alert("INCORRECT!")
 }
-if (tall === "no") {
+if (tall == "no" ) {
   alert("CORRECT!")
+  score++
 }
+console.log("Current score: " + score)
+
+var pickles = prompt("Does Nathan love pickles?").toLowerCase();
+console.log(pickles);
+if (pickles == "no" ) {
+  alert("INCORRECT!")
+}
+if (pickles == "yes" ) {
+  alert("CORRECT!")
+  score++
+}
+console.log("Current score: " + score)
+
+var vampire = prompt("Is Nathan secretly a vampire?").toLowerCase();
+console.log(vampire);
+if (vampire == "yes" ) {
+  alert("INCORRECT!")
+}
+if (vampire == "no" ) {
+  alert("CORRECT!")
+  score++
+}
+console.log("Current score: " + score)
+
+var helicopter = prompt("Can Nathan safely pilot a helicopter?").toLowerCase();
+console.log(helicopter);
+if (helicopter == "yes" ) {
+  alert("INCORRECT!")
+}
+if (helicopter == "no" ) {
+  alert("CORRECT!")
+  score++
+}
+console.log("Current score: " + score)
+
+var mommy = prompt("Does Nathan love his mommy?").toLowerCase();
+console.log(mommy);
+if (mommy == "no" ) {
+  alert("INCORRECT!")
+}
+if (mommy == "yes" ) {
+  alert("CORRECT!")
+  score++
+}
+console.log("Current score: " + score)
+
+alert("Your final score is " + score + " out of 5!");
