2f3ee75c9f3811b1706b1dfb7f7a1f13ccadfbc7
unknown <ncmoore1986@gmail.com>
2017-08-08 14:23:53 -0700

    Now my first question is working how I want it to

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict'
 
-alert("Hello! I am an alert box!!");
+alert("Welcome to the EXTREME CHEDDAR guessing game!!");
+var tall = prompt("Is Nathan 20 feet tall?");
+tall.toLowerCase();
+if (tall === "yes") {
+  alert("INCORRECT!")
+}
+if (tall === "no") {
+  alert("CORRECT!")
+}
