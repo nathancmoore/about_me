b21c74a13b98e099bd9bf7a1353021981c379c19
allstot <n.allstot@gmail.com>
2017-08-10 13:59:23 -0700

    Functions and arrays for Q6

--- app.js
+++ app.js
@@ -10,106 +10,59 @@ var questions = ['Is Nathan more than 20 feet tall?', 'Does Nathan love pickles?
 
 var answers = ['no', 'n', 'yes', 'y', 'no', 'n','no', 'n', 'yes', 'y'];
 
-var responses = ['INCORRECT!', 'CORRECT!'];
+var responses1 = ['INCORRECT!', 'CORRECT!'];
 
-var userInput;
+var responses2 = ['DINGDINGDING! Correct!', 'Invalid entry! Try again!', 'Too Low! Try again.','Too High! Try again.'];
 
-function quizOne (userInput) {
+var userInput1;
+
+var userInput2;
+
+var correctNum = (Math.floor(Math.random() * 100)) + 1;
+
+function quizOne (userInput1) {
   for (var qcount = 0; qcount < 5; qcount ++) {
     var acount = qcount * 2;
-    userInput = prompt(questions[qcount]);
-    console.log(userInput);
-    console.log(userInput.toLowerCase());
+    userInput1 = prompt(questions[qcount]);
+    console.log(userInput1);
+    console.log(userInput1.toLowerCase());
     console.log('acount is ' + acount);
     console.log('qcount is ' + qcount);
-    if (userInput.toLowerCase() === answers[acount] || userInput.toLowerCase() === answers[acount + 1]) {
-      alert(responses[1]);
+    if (userInput1.toLowerCase() === answers[acount] || userInput1.toLowerCase() === answers[acount + 1]) {
+      alert(responses1[1]);
       score++;
       console.log('Current score: ' + score);
     }
     else {
-      alert(responses[0]);
+      alert(responses1[0]);
+    }
+  }
+}
+
+function quizTwo (userInput2) {
+  console.log('The random number is ' + correctNum + '.');
+  for (var attemptsNum = 0; attemptsNum < 4; attemptsNum++) {
+    var userInput2 = prompt('Choose an integer between 1-100. You have ' + (4 - attemptsNum) + ' guess(es) remaining!');
+    console.log('The number selected by the user is ' + userInput2 + '.');
+    if (userInput2 == correctNum) {
+      alert(responses2[0]);
+      score++;
+      console.log('Current score: ' + score);
+      break;
+    } else if (userInput2 > 100 || userInput2 < 1 || (userInput2 - Math.floor(userInput2)) != 0) {
+      alert(responses2[1]);
+      continue;
+    } else if (correctNum > userInput2) {
+      alert(responses2[2]);
+    } else if (correctNum < userInput2) {
+      alert(responses2[3]);
     }
   }
 }
 
 quizOne();
+quizTwo();
 
-// var tall = prompt('Is Nathan more than 20 feet tall?').toLowerCase();
-// console.log(tall);
-// if (tall == 'yes' || tall == 'y') {
-//   alert('INCORRECT!');
-// }
-// if (tall == 'no' || tall == 'n') {
-//   alert('CORRECT!');
-//   score++;
-// }
-// console.log('Current score: ' + score);
-//
-// var pickles = prompt('Does Nathan love pickles?').toLowerCase();
-// console.log(pickles);
-// if (pickles == 'no' || pickles == 'n') {
-//   alert('INCORRECT!');
-// }
-// if (pickles == 'yes' || pickles == 'y') {
-//   alert('CORRECT!');
-//   score++;
-// }
-// console.log('Current score: ' + score);
-//
-// var vampire = prompt('Is Nathan secretly a vampire?').toLowerCase();
-// console.log(vampire);
-// if (vampire == 'yes' || vampire == 'y') {
-//   alert('INCORRECT!');
-// }
-// if (vampire == 'no' || vampire == 'n') {
-//   alert('CORRECT!');
-//   score++;
-// }
-// console.log('Current score: ' + score);
-//
-// var helicopter = prompt('Can Nathan safely pilot a helicopter?').toLowerCase();
-// console.log(helicopter);
-// if (helicopter == 'yes' || helicopter == 'y') {
-//   alert('INCORRECT!');
-// }
-// if (helicopter == 'no' || helicopter == 'n') {
-//   alert('CORRECT!');
-//   score++;
-// }
-// console.log('Current score: ' + score);
-//
-// var mommy = prompt('Does Nathan love his mommy?').toLowerCase();
-// console.log(mommy);
-// if (mommy == 'no' || mommy == 'n') {
-//   alert('INCORRECT!');
-// }
-// if (mommy == 'yes' || mommy == 'y') {
-//   alert('CORRECT!');
-//   score++;
-// }
-// console.log('Current score: ' + score);
-//
-// var correctNum = (Math.floor(Math.random() * 100)) + 1;
-// console.log('The random number is ' + correctNum + '.');
-//
-// for (var attemptsNum = 0; attemptsNum < 4; attemptsNum++) {
-//   var userNum = prompt('Choose an integer between 1-100. You have ' + (4 - attemptsNum) + ' guess(es) remaining!');
-//   console.log('The number selected by the user is ' + userNum + '.');
-//   if (userNum == correctNum) {
-//     alert('DINGDINGDING! Correct!');
-//     score++;
-//     console.log('Current score: ' + score);
-//     break;
-//   } else if (userNum > 100 || userNum < 1 || (userNum - Math.floor(userNum)) != 0) {
-//     alert('Invalid entry! Try again!');
-//     continue;
-//   } else if (correctNum > userNum) {
-//     alert('Too Low! Try again.');
-//   } else if (correctNum < userNum) {
-//     alert('Too High! Try again.');
-//   }
-// }
 //
 // var correctCar = ['honda', 'toyota', 'ford', 'volvo'];
 // console.log('Correct car brands are ' + correctCar + '.');
